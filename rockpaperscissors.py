ccea44bcd60d39a12d5008c85a2ad898d6d7a1ef
sanjaygaire002 <160398064+sanjaygaire002@users.noreply.github.com>
2024-02-23 09:11:08 +0545

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,50 @@
+def r_p_s_game():
+    import random
+    ch=("R","P","S")
+    computer=random.choice(ch)
+    print("----------------------------------")
+    print("=====ROCK-PAPER-SCISSORS-GAME=====")
+    print("----------------------------------")
+    user="a"
+    u_num=0
+    c_num=0
+    while True:
+        while user not in ch:
+            uc= input("enter 'R' for ROCK, 'P' for PAPER and 'S' for SCISSORS \n ----------------------------------\n")
+            user=uc.upper()
+            print(f"=====Player: {user}=====\n=====Computer: {computer}=====")
+            #print(user)
+        if user == computer:
+            print("=====TIE=====")
+            u_num+=1
+            c_num+=1
+        elif(user == "R" and computer == "S"):
+            print("=====YOU WIN=====")
+            u_num+=1
+        elif (user == "S" and computer == "P"):
+            print("=====YOU WIN=====")
+            u_num+=1
+        elif (user == "P" and computer == "R"):
+            print("=====YOU WIN=====")
+            u_num+=1
+        else:
+            print("=====YOU LOOSE=====")
+            c_num+=1
+        print("----------------------------------")
+        print("=====SCORE=====") 
+        print(f"--------------USER: {u_num}") 
+        print(f"--------------COMPUTER: {c_num}")
+        print("\n----------------------------------")
+        cont=input("=====DO YOU WANT TO CONTINUE(Y/N)=====").upper()
+        if cont=="Y":
+            continue
+        else:
+            
+            if u_num> c_num:
+                 print("======YOU WIN=====")
+            if u_num== c_num:
+                print("=====TIE=====")
+            if u_num< c_num:
+                print("=====YOU LOOSE=====")
+            break
+r_p_s_game()
